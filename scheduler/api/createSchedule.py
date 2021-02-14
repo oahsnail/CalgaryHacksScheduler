f1a223918ca3437b9ffda2d7cc425c0a235d9270
Lian Shao <lianshao2000@gmail.com>
2021-02-14 02:32:08 -0800

    fixed depency error in createSchedule

--- scheduler/api/createSchedule.py
+++ scheduler/api/createSchedule.py
@@ -2,37 +2,43 @@ import datetime
 import api
 import math
 
-TIME_CHUNK = 15 #the size (in minutes) of the invidually alloted chunks of time
+# the size (in minutes) of the invidually alloted chunks of time
+TIME_CHUNK = 15
 
 
 def createSchedule(user):
-    myTasks = api.models.Task.objects.filter(user = user).order_by('dueDate') #grabs only the users tasks ans filters them by due date
+    myTasks = api.models.Task.objects.filter(user=user).order_by(
+        'dueDate')  # grabs only the users tasks ans filters them by due date
     lastTaskTime = findLastTask(user)
-    arraySize = int(math.ceil((lastTaskTime-datetime.datetime.now())/datetime.timedelta(minutes=TIME_CHUNK)))
+    arraySize = int(math.ceil(
+        (lastTaskTime-datetime.datetime.now())/datetime.timedelta(minutes=TIME_CHUNK)))
     scheduleArray = [None]*arraySize
 
-    #grabs the QuerySet and turns it into a list
+    # grabs the QuerySet and turns it into a list
     regularTaskList = list()
     for task in myTasks:
         regularTaskList.append(task)
 
-    #subdivides tasks longer than TIME_CHUNK into many TIME_CHUNK sized tasks
+    # subdivides tasks longer than TIME_CHUNK into many TIME_CHUNK sized tasks
     subdividedTaskList = list()
     for task in regularTaskList:
         if task.taskLength < TIME_CHUNK and task.taskLength > 0:
             subdividedTaskList.append(task)
         elif task.taskLength > 0:
-            tempTask = api.models.Task(id=api.models.generate_unique_task_id(), taskName = task.taskName, dueDate=(task.dueDate-datetime.timedelta(minutes=TIME_CHUNK)),user = task.user, taskLength = task.taskLength -TIME_CHUNK)
+            tempTask = api.models.Task(id=api.models.generate_unique_task_id(), taskName=task.taskName, dueDate=(
+                task.dueDate-datetime.timedelta(minutes=TIME_CHUNK)), user=task.user, taskLength=task.taskLength - TIME_CHUNK)
             regularTaskList.append(tempTask)
-            tempTask = models.Task(id=models.generate_unique_task_id(
+            tempTask = api.models.Task(id=api.models.generate_unique_task_id(
             ), taskName=task.taskName, dueDate=task.dueDate, user=task.user, taskLength=TIME_CHUNK)
             subdividedTaskList.append(tempTask)
 
     for task in subdividedTaskList:
-        print("Name: "+ str(task.taskName) +"   DueDate: "+ str(task.dueDate) + "   taskLength: " + str(task.taskLength))
+        print("Name: " + str(task.taskName) + "   DueDate: " +
+              str(task.dueDate) + "   taskLength: " + str(task.taskLength))
 
     print(arraySize)
 
+
 def findLastTask(user):
-    lastTask = api.models.Task.objects.filter(user = user).latest('dueDate')
+    lastTask = api.models.Task.objects.filter(user=user).latest('dueDate')
     return lastTask.dueDate
