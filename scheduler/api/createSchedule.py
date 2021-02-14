aaa4e4685f439effbdffcc2e84b1e9204a36c3a3
Lian Shao <lianshao2000@gmail.com>
2021-02-14 01:28:31 -0800

    fixed createSchedule imports

--- scheduler/api/createSchedule.py
+++ scheduler/api/createSchedule.py
@@ -1,13 +1,14 @@
-from .models import Room, User, Task,
+from .models import *
 import datetime
-import api
 
 TIME_CHUNK = 15
 
+
 def createSchedule(user):
-    myTasks = api.models.Task.objects.filter(user = user).order_by('dueDate')
+    myTasks = models.Task.objects.filter(user=user).order_by('dueDate')
     lastTaskTime = findLastTask(user)
-    arraySize = int(((lastTaskTime-datetime.datetime.now())/datetime.timedelta(minutes=TIME_CHUNK)))
+    arraySize = int(((lastTaskTime-datetime.datetime.now()) /
+                     datetime.timedelta(minutes=TIME_CHUNK)))
     scheduleArray = [None]*arraySize
     regularTaskList = list()
     for task in myTasks:
@@ -17,17 +18,15 @@ def createSchedule(user):
     for task in regularTaskList:
         if task.taskLength < TIME_CHUNK:
             subdividedTaskList.append(task)
-        else if task.taskLength > 0:
-            tempTask = api.models.Task(id=api.models.generate_unique_task_id(), taskName = task.taskName, dueDate=(task.dueDate-datetime.timedelta(minutes=TIME_CHUNK)),user = task.user, taskLength = task.taskLength -TIME_CHUNK)
+        elif task.taskLength > 0:
+            tempTask = models.Task(id=models.generate_unique_task_id(), taskName=task.taskName, dueDate=(
+                task.dueDate-datetime.timedelta(minutes=TIME_CHUNK)), user=task.user, taskLength=task.taskLength - TIME_CHUNK)
             regularTaskList.append(tempTask)
-            tempTask = api.models.Task(id=api.models.generate_unique_task_id(), taskName = task.taskName, dueDate=task.dueDate,user = task.user, taskLength = TIME_CHUNK)
+            tempTask = models.Task(id=models.generate_unique_task_id(
+            ), taskName=task.taskName, dueDate=task.dueDate, user=task.user, taskLength=TIME_CHUNK)
             subdividedTaskList.append(tempTask)
-            
 
-
-        
-        
 
 def findLastTask(user):
-    lastTask = Task.objects.filter(user = user).latest('dueDate')
+    lastTask = Task.objects.filter(user=user).latest('dueDate')
     return lastTask.dueDate
